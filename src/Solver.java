570bdde3bbe26bf104a904694f19f068aaff006c
Stephen Dunn <dunn.stephen@gmail.com>
2019-10-28 10:46:36 -0700

    flipped args

--- src/Solver.java
+++ src/Solver.java
@@ -121,9 +121,9 @@ public class Solver {
 
     public static void main(String[] args)
     {
-        int runs = Integer.parseInt(args[0]);
-        SlidingTilePuzzle puzzle = new SlidingTilePuzzle(Integer.parseInt(args[1]));
+        SlidingTilePuzzle puzzle = new SlidingTilePuzzle(Integer.parseInt(args[0]));
         Solver solver = new Solver(puzzle);
+        int runs = Integer.parseInt(args[1]);
 
         puzzle.print();
         System.out.println("runs: " + runs + "\nheuristic: avg moves");
